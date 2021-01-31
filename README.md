982b38acddc76015d495387c21465b5dfd678c1c
rakshaa2000 <rakshaa2000@gmail.com>
2021-01-31 17:18:09 +0530

    Changes

---
+++ README.md
@@ -0,0 +1,2 @@
+# NoRacism
+ 
---
+++ src/main.py
@@ -0,0 +1,72 @@
+import os
+import discord
+import random
+from trie.Trie import Trie
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+client = discord.Client()
+trie = Trie()
+table = {
+    "\"": None,
+    "'": None,
+    "-": None,
+    "`": None,
+    "~": None,
+    ",": None,
+    ".": None,
+    ":": None,
+    ";": None,
+    "_": None
+}
+
+
+def buildTrie():
+    file = open("trie/words.txt", 'r')
+
+    for line in file:
+        line = line.strip()
+        trie.insert(line)
+
+
+def punish_user(user_id):
+    user_id = '<@' + str(user_id) + '>'
+    responses = [
+        "{} - We don't use racist slurs here, or anywhere",
+        "Hey now {}, watch your mouth.",
+        "We don't use that kind of language here, {}."
+    ]
+
+    choice = random.choice(responses)
+    choice = choice.format(user_id)
+
+    return choice
+
+
+@client.event
+async def on_ready():
+    buildTrie()
+    print("Trie is built. ready to read messages.")
+
+
+@client.event
+async def on_message(message):
+    text = message.content
+    text = text.translate(str.maketrans(table))
+    author_id = message.author.id
+
+    if author_id != 756276859225768057:
+        isClean = True
+        message_word_list = text.split()
+        for word in message_word_list:
+            if trie.search(word):
+                isClean = False
+                break
+        if not isClean:
+            await message.delete()
+            await message.channel.send(punish_user(author_id))
+
+
+client.run(os.getenv('TOKEN'))
